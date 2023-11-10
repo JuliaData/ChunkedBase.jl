5dbb280fad60b5bd136b426b52197821421d09b5
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2023-11-10 14:53:23 +0100

    Tweak comment

--- src/payload.jl
+++ src/payload.jl
@@ -10,7 +10,7 @@ A payload of parsed results, which is passed to `consume!` after each `populate_
 # Fields:
 - `row_num::Int`: row number of the first row in the payload
 - `len::Int`: number of rows in the payload
-- `results::B`: parsed result buffer
+- `results::B`: parsed result buffer which was populated by `populate_result_buffer!`
 - `parsing_ctx::C`: library-provided data (to distinguish JSONL and CSV processing)
 - `chunking_ctx::ChunkingContext`: contains the raw bytes, synchronization objects and newline positions
 - `eols_buffer_index::Int32`: The start index of the newline positions in `chunking_ctx.newline_positions` that this payload corresponds to.
@@ -21,7 +21,7 @@ A payload of parsed results, which is passed to `consume!` after each `populate_
 struct ParsedPayload{B, C<:AbstractParsingContext}
     row_num::Int                  # row number of the first row in the payload
     len::Int                      # number of rows in the payload
-    results::B                    # parsed result buffer
+    results::B                    # parsed result buffer which was populated by `populate_result_buffer!`
     parsing_ctx::C                # library-provided data (to distinguish JSONL and CSV processing)
     chunking_ctx::ChunkingContext # internal data to facilitate chunking and synchronization
     eols_buffer_index::Int32      # The start index of the newline positions in `chunking_ctx.newline_positions` that this payload corresponds to.
